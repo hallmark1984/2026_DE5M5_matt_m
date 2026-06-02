96b24de9dcccdfcfa0296f2a5488b6e6d06ef919
hallmark1984 <matt.mcaleavy@gmail.com>
2026-06-02 11:54:19 +0100

    up to date

---
+++ tests/tests.py
@@ -0,0 +1,23 @@
+import unittest
+from calculator import Calculator
+
+class TestOperations(unittest.TestCase):
+    def __init__(self):
+        self.calculator = Calculator(num1=8,num2=2, matrix_1=[1,1], matrix_2=[1,1])
+
+    def test_sum(self):
+        self.assertEqual(self.calculator.get_sum(),10,'The sum was not 10')
+
+    def test_subtract_1(self):
+        self.assertEqual(self.calculator.get_subtract_a(),6,'The return was not 6')
+
+    def test_subtract_2(self):
+        self.assertEqual(self.calculator.get_subtract_b(),-6,'The return was not -6')
+
+    def test_divide(self):      
+        self.assertEqual(self.calculator.get_quotient_a(),0.25,'The return was not 0.25')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
